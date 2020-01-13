10482fb0b434c230506e542a82808baffb015eea
Edvin Forsum <edvin.forsum@learnet.se>
2020-01-13 11:03:02 +0100

    Hopefully fixed transfering of levels, however I havent tried this yet since I only have 1 computer atm

--- CloneDroneModdedMultiplayer/Internal/ServerRunner.cs
+++ CloneDroneModdedMultiplayer/Internal/ServerRunner.cs
@@ -47,7 +47,7 @@ namespace CloneDroneModdedMultiplayer.Internal
             });
             
         }
-        public const int SEND_LEVEL_BYTES_PER_MSG = 512;
+        
         public const string TEMP_MAP_NAME = "tempLevel.json";
 
         public static void StartClient(string ip, int port = 606)
@@ -119,101 +119,31 @@ namespace CloneDroneModdedMultiplayer.Internal
 
         public static string ReciveMap(Socket socket)
         {
-            byte[] buffer = new byte[SEND_LEVEL_BYTES_PER_MSG];
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log(0); });
+            byte[] lengthBytes = new byte[sizeof(int)];
+            socket.Receive(lengthBytes);
+            socket.Send(lengthBytes);
+            int length = BitConverter.ToInt32(lengthBytes, 0);
+            byte[] buffer = new byte[length];
             socket.Receive(buffer);
-            short length = BitConverter.ToInt16(new byte[] { buffer[0], buffer[1] }, 0);
-            byte[][] reconstructedFileStep1 = new byte[length][];
-            reconstructedFileStep1[BitConverter.ToInt16(new byte[] { buffer[2], buffer[3] }, 0)] = buffer;
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log(1); });
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log("length: " + length); });
-            for(int i = 1; i < length; i++)
-            {
-                socket.Receive(buffer);
 
-                for(int j = 0; j < buffer.Length; j++)
-                {
-                    Console.Write(j + ": ");
-                    Console.Write(buffer[j]);
-                    Console.WriteLine();
-                }
-
-                short index = BitConverter.ToInt16(new byte[] { buffer[2], buffer[3] }, 0);
-                NetworkingCore.ScheduleForMainThread(delegate { debug.Log("index: " + index); });
-                reconstructedFileStep1[index] = buffer;
-            }
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log(2); });
-            int endIndex = -1;
-            byte[] array = reconstructedFileStep1[length-1];
-            for(int i = 0; i < SEND_LEVEL_BYTES_PER_MSG; i++)
-            {
-                if(array[i] == 0)
-                {
-                    endIndex = i;
-                    break;
-                }
-            }
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log(3); });
-            int fileLength = reconstructedFileStep1.Length * (SEND_LEVEL_BYTES_PER_MSG-4) - (SEND_LEVEL_BYTES_PER_MSG - endIndex);
-            byte[] reconstructedFileStep2 = new byte[fileLength];
-            int currrentIndex = 0;
-            for(int i = 0; i < reconstructedFileStep1.Length; i++)
-            {
-                for(int j = 4; j < reconstructedFileStep1[j].Length; j++) // skip the first 4 since they are reserved
-                {
-                    if(currrentIndex < reconstructedFileStep2.Length)
-                    {
-                        reconstructedFileStep2[currrentIndex] = reconstructedFileStep1[i][j];
-                    }
-                    currrentIndex++;
-                }
-            }
-            NetworkingCore.ScheduleForMainThread(delegate { debug.Log(4); });
-            string path = Application.persistentDataPath + "/ModdedLevels/" + TEMP_MAP_NAME;
-            File.Create(path);
+            string path = Application.dataPath + "/ModdedLevels/"+TEMP_MAP_NAME;
+            File.WriteAllBytes(path, buffer);
             return path;
         }
         public static void SendMap(Socket socket, string jsonPath)
         {
             byte[] fileBytes = File.ReadAllBytes(jsonPath);
-            int bytesLength = fileBytes.Length;
 
-            int msgCount = (byte)Math.Ceiling(bytesLength/(float)(SEND_LEVEL_BYTES_PER_MSG-4));
+            byte[] lengthAsBytes = BitConverter.GetBytes(fileBytes.Length);
 
-            if(msgCount > short.MaxValue)
-            {
-                NetworkingCore.ScheduleForMainThread(delegate { Debug.LogError("level too big :/ length: " + bytesLength); });
-                return;
-            }
+            socket.Send(lengthAsBytes);
+            byte[] buffer = new byte[sizeof(int)];
+            socket.Receive(buffer);
+            if(buffer != lengthAsBytes)
+                throw new Exception("Something went wrong");
 
+            socket.Send(fileBytes);
 
-            byte[] buffer = new byte[SEND_LEVEL_BYTES_PER_MSG];
-            for(short i = 0; i < msgCount; i++)
-            {
-                byte[] msgCountBytes = BitConverter.GetBytes((short)msgCount);
-                buffer[0] = msgCountBytes[0];
-                buffer[1] = msgCountBytes[1];
-                byte[] iBytes = BitConverter.GetBytes(i);
-                buffer[2] = iBytes[0];
-                buffer[3] = iBytes[1];
-
-                for(int j = 4; j < SEND_LEVEL_BYTES_PER_MSG; j++)
-                {
-                    int offset = i*(SEND_LEVEL_BYTES_PER_MSG-4) + j - 4; // calculates the offset in the fileBytes array, -2 since the 2 first bytes are reserved for total length and current index
-
-                    if(offset >= fileBytes.Length) // if the file has ended we dont want to get an index out of range exception
-                    {
-                        buffer[j] = 0;
-                    }
-                    else
-                    {
-                        buffer[j] = fileBytes[offset];
-                    }
-
-                }
-
-                socket.Send(buffer);
-            }
 
         }
     }
